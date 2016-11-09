8cc269153037048738d3ef6ea55c8e9b2f5ccdb6
Jesse Mykolyn <jrmykolyn@gmail.com>
2016-11-09 00:29:07 -0500

    Completed first pass of 'bubble sort'.

--- js/main.js
+++ js/main.js
@@ -23,6 +23,58 @@ $( document ).ready( function() {
 		return arr;
 	}
 
+
+	/**
+	 * ...
+	 */
+	function doBubbleSort( arr ) {
+		// Create duplicate of `arr` arg. via `.slice()`.
+		arr = arr.slice(0);
+
+		// Assign `counter` in order to:
+		// - record total loop iterations;
+		// - have value to check against when attempting to exit loop.
+		var counter = 0;
+
+		// Initialize `performed_swap` to determine whether a
+		// loop featured no 'swaps'.
+		var performed_swap;
+
+		// Repeat the block below until either:
+		// - the `counter` var. is equal to the length of the input minus 1;
+		// - OR
+		// - No 'swaps' were performed during one loop over the input array.
+		while ( counter < ( arr.length - 1 ) && performed_swap !== false ) {
+			// Reset `performed_swap` to `false` at the beginning of each 'outer pass'
+			performed_swap = false;
+
+			// Loop over the array, excluding the last member.
+			for ( var i = 0, x = ( arr.length - 1 ); i < x; i++ ) {
+				// Store the values at the current and next indices.
+				var current_val = arr[i],
+					next_val = arr[i + 1];
+
+				// If the 'current' value is greater than the 'next' value,
+				// 'swap' them and update the `performed_swap` var.
+				if ( current_val > next_val ) {
+					arr[i] = next_val;
+					arr[i + 1] = current_val;
+
+					performed_swap = true;
+				}
+			}
+
+			// Update `counter`
+			counter++;
+		}
+
+		// Return the sorted array.
+		return arr;
+	}
+
+	/**
+	 * ...
+	 */
 	function doInsertionSort( arr ) {
 		// Create duplicate of `arr` arg. via `.slice()`.
 		arr = arr.slice(0);
@@ -63,15 +115,15 @@ $( document ).ready( function() {
 	/* -------------------------------------------------- */
 	/* INIT */
 	/* -------------------------------------------------- */
-	var arr1_sorted = doInsertionSort( arr1 );
+	var arr1_sorted = doBubbleSort( arr1 );
 	console.log( arr1 ); /// TEMP
 	console.log( arr1_sorted ); /// TEMP
 
-	var arr2_sorted = doInsertionSort( arr2 );
+	var arr2_sorted = doBubbleSort( arr2 );
 	console.log( arr2 ); /// TEMP
 	console.log( arr2_sorted ); /// TEMP
 
-	var arr3_sorted = doInsertionSort( arr3 );
+	var arr3_sorted = doBubbleSort( arr3 );
 	console.log( arr3 ); /// TEMP
 	console.log( arr3_sorted ); /// TEMP
 
